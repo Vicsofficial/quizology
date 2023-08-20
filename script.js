7a48028f053b56f15e4ccead514cf6e70722710e
Vic <132408212+Vicsofficial@users.noreply.github.com>
2023-08-20 17:49:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,109 @@
-const highScore = "";
-const countDown = "";
-let questions = ["Question 1", "Question 2", "Question 3"];
-let options = ["Option 1", "Option 2", "Option 3", "Option 4"];
-let rightAnswer = "";
-let timer = 6000;
-var questionIndex;
 
-// List of questions
-var questionBank = [
+
+const startButton = document.getElementById('start-button');
+const questionContainer = document.getElementById('question-container');
+const answerOptions = document.querySelectorAll('.answer');
+const timerElement = document.getElementById('time-remaining');
+const gameOverContainer = document.getElementById('game-over-container');
+const initialsInput = document.getElementById('initials');
+const saveScoreButton = document.getElementById('save-score');
+
+let currentQuestionIndex = 0;
+let timeRemaining = 60; // Timer value in seconds
+let timerInterval;
+
+const questions = [
   {
-    prompt: "Question 1",
-    choices: ["answer a", "answer b", "answer c", "answer d"],
-    correctAnswer: "answer c",
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: ['<js>', '<java>', '<javascript>', '<script>'],
+    correctAnswer: 3,
   },
+
   {
-    prompt: "Question 2",
-    choices: ["answer a", "answer b", "answer c", "answer d"],
-    correctAnswer: "answer b",
+    question: 'Where is the correct place to insert a JavaScript?',
+    answers: ['<body>', '<head>', 'footer', 'both <head and <body>'],
+    correctAnswer: 0,
   },
+
   {
-    prompt: "Question 2",
-    choices: ["answer a", "answer b", "answer c", "answer d"],
-    correctAnswer: "answer a",
+    question: 'The external JavaScript file must contain the <script> tag',
+    answers: ['True', 'False'],
+    correctAnswer: 1,
   },
-  {
-    prompt: "Question 2",
-    choices: ["answer a", "answer b", "answer c", "answer d"],
-    correctAnswer: "answer d",
-  },
-  {
-    prompt: "Question 2",
-    choices: ["answer a", "answer b", "answer c", "answer d"],
-    correctAnswer: "answer a",
-  },
+  
 ];
 
-// Get started button click starts the time function here
+startButton.addEventListener('click', startQuiz);
+saveScoreButton.addEventListener('click', saveScore);
 
-// function 1
 function startQuiz() {
-  document.getElementById("start-button").addEventListener("click", startQuiz);
-  console.log("Starting the quiz");
-  questionIndex = 0;
+  startButton.style.display = 'none';
+  quizContainer.style.display = 'block';
   startTimer();
-  showQuestion();
+  displayQuestion();
 }
 
-//function 2
-function showQuestion() {
-  console.log("show question");
-  var currentQuestion = questionBank[questionIndex];
-  // existing element in the html
-  // h3 element to show question prompt
-  // Add unordered list to html document
-  // Target the option choice container Element
-  // we have our choices array --iterate through it
-  // im going to do something for each choice -> create a button, populate its content, add event listener, and append it to choices Element
+function startTimer() {
+  timerInterval = setInterval(() => {
+    timeRemaining--;
+    timerElement.textContent = timeRemaining;
+    if (timeRemaining <= 0) {
+      endQuiz();
+    }
+  }, 1000);
 }
 
-// function 3
-function checkAnswer(event) {
-  console.log(event);
-  console.log(event.target);
-  console.dir(event.target);
-  // deduct time if incorrect answer
-  var currentQuestion = questionBank[questionIndex];
-
-  // have to decide when you can move to the next question, or if you will call endQuiz()
-  // after you're done checking the answer, you will move questionIndex ++ and call showQuestion()
+function displayQuestion() {
+  const currentQuestion = questions[currentQuestionIndex];
+  document.getElementById('question').textContent = currentQuestion.question;
+  answerOptions.forEach((option, index) => {
+    option.textContent = currentQuestion.answers[index];
+    option.addEventListener('click', () => checkAnswer(index));
+  });
 }
 
-//function 4
-function endQuiz() {}
+function checkAnswer(selectedIndex) {
+  const currentQuestion = questions[currentQuestionIndex];
+  if (selectedIndex === currentQuestion.correctAnswer) {
+    // Correct answer
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
+    }
+  } else {
+    // Incorrect answer
+    timeRemaining -= 10;
+    if (timeRemaining <= 0) {
+      endQuiz();
+    }
+  }
+}
 
+function endQuiz() {
+  clearInterval(timerInterval);
+  quizContainer.style.display = 'none';
+  gameOverContainer.style.display = 'block';
+}
 
+function saveScore() {
+  const initials = initialsInput.value;
+  const score = timeRemaining;
+}
 
+function saveScore() {
+  const initials = initialsInput.value;
+  const score = timeRemaining;
+
+  const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+  highScores.push({initials, score });
+
+  localStorage.setItem('highScores', JSON.stringify(highScores));
+
+  initialsInput.value = '';
+}
 
 
 
